60ff2a39dfde3eba804ffca39824a13f24b0c155
Daniel James Szelogowski <dan@zerodevelopment.net>
2020-06-09 21:58:37 -0500

    Added new Unit Test

--- Unit Test/UnitTest.cs
+++ Unit Test/UnitTest.cs
@@ -119,5 +119,81 @@ namespace Unit_Test
             Console.WriteLine(time1 < time3 ? "Chunk List ran " + Math.Round((time3 / time1), 2) + "x faster than Sqrt Chunk List" : "Chunk List ran " + Math.Round((time1 / time3), 2) + "x slower than Sqrt Chunk List");
         }
 
+        [Fact]
+        public void ChunkSizeComparison()
+        {
+            var list0 = new ChunkList<int>(10);
+            var list1 = new ChunkList<int>(100);
+            var list2 = new ChunkList<int>(500);
+            var list3 = new ChunkList<int>(1000);
+            var list4 = new ChunkList<int>(2500);
+            var list5 = new ChunkList<int>(5000);
+            var list6 = new ChunkList<int>(10000);
+            var list7 = new ChunkList<int>(25000);
+            var list8 = new ChunkList<int>(50000);
+            var list9 = new ChunkList<int>(100000);
+            var list10 = new ChunkList<int>(500000);
+            
+            Random rand = new Random();
+
+            for (int i = 0; i < 500000; i++)
+            {
+                int x = rand.Next(0, 10);
+                list0.add(x);
+                list1.add(x);
+                list2.add(x);
+                list3.add(x);
+                list4.add(x);
+                list5.add(x);
+                list6.add(x);
+                list7.add(x);
+                list8.add(x);
+                list9.add(x);
+                list10.add(x);
+            }
+
+            Stopwatch Test(ChunkList<int> list)
+            {
+                Console.WriteLine("Testing list of 500,000 items with chunk size of {0}", list.getChunkSize());
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                
+                //Console.WriteLine("\n\n{0}\n{1}\n{2}\n\nSize: {3}\n\n", list.contains(3), list.contains(6), list.contains(500), list.size());
+                //Console.WriteLine("[Elapsed time] Minutes: " + stopwatch.Elapsed.Minutes + " Seconds: " + stopwatch.Elapsed.Seconds + "." + stopwatch.Elapsed.Milliseconds);
+                //Console.WriteLine("Sorting & Removing:\n\n");
+                
+                list.sort();
+                
+                list.remove(7);
+                list.removeAll(3);
+
+                stopwatch.Stop();
+            
+                //Console.WriteLine("Completely Done\n[Elapsed time] Minutes: " + stopwatch.Elapsed.Minutes + " Seconds: " + stopwatch.Elapsed.Seconds + "." + stopwatch.Elapsed.Milliseconds);    
+                //Console.WriteLine("\n******************************************************************\n");
+
+                return stopwatch;
+            }
+
+            var resultArray = new[]
+            {
+                new {chunkSize = list0.getChunkSize(), time = Test(list0)},
+                new {chunkSize = list1.getChunkSize(), time = Test(list1)},
+                new {chunkSize = list2.getChunkSize(), time = Test(list2)},
+                new {chunkSize = list3.getChunkSize(), time = Test(list3)},
+                new {chunkSize = list4.getChunkSize(), time = Test(list4)},
+                new {chunkSize = list5.getChunkSize(), time = Test(list5)},
+                new {chunkSize = list6.getChunkSize(), time = Test(list6)},
+                new {chunkSize = list7.getChunkSize(), time = Test(list7)},
+                new {chunkSize = list8.getChunkSize(), time = Test(list8)},
+                new {chunkSize = list9.getChunkSize(), time = Test(list9)},
+                new {chunkSize = list10.getChunkSize(), time = Test(list10)}
+            };
+
+            foreach (var result in resultArray)
+            {
+                Console.WriteLine("Result for chunk size of " + result.chunkSize + ": " + result.time.Elapsed.Minutes + " Minutes, " + result.time.Elapsed.Seconds + "." + result.time.Elapsed.Milliseconds + " Seconds");
+            } 
+        }
     }
 }
